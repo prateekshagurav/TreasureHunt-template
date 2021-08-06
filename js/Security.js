8ef957f9cb8d2c516c63beed31cbb0e80c197713
prateekshagurav <74418674+prateekshagurav@users.noreply.github.com>
2021-08-06 17:21:44 +0530

    Add files via upload

---
+++ js/Security.js
@@ -0,0 +1,55 @@
+class Security {
+
+    constructor() {
+
+        this.access1 = createInput("Code1")
+        this.access1.position(100, 90);
+        this.access1.style('background', 'white');
+
+        this.button1 = createButton('Check');
+        this.button1.position(100, 120);
+        this.button1.style('background', 'lightgrey');
+
+        this.access2 = createInput("Code1")
+        this.access2.position(700, 190);
+        this.access2.style('background', 'white');
+
+        this.button2 = createButton('Check');
+        this.button2.position(700, 220);
+        this.button2.style('background', 'lightgrey');
+
+        this.access3 = createInput("Code1")
+        this.access3.position(100, 290);
+        this.access3.style('background', 'white');
+
+        //create button3 here
+
+
+
+    }
+
+    display() {
+
+        this.button1.mousePressed(() => {
+            if (system.authenticate(accessCode1, this.access1.value()))
+            {
+                this.button1.hide();
+                this.access1.hide();
+                score++;
+            }
+        });
+
+        this.button2.mousePressed(() => {
+            if (system.authenticate(accessCode2, this.access2.value()))
+            {
+                this.button2.hide();
+                this.access2.hide();
+                score++;
+            }
+        });
+
+        //code when button 3 is pressed
+
+
+    }
+}
